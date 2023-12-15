87d3280fa42cabd6d29082f2ebf50897b3908e2d
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-12-15 09:59:54 -0500

    MovedRequestMapping methods to Controller Class

--- src/main/java/com/hcc/controllers/AssignmentController.java
+++ src/main/java/com/hcc/controllers/AssignmentController.java
@@ -1,7 +1,8 @@
 package com.hcc.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hcc.entities.Assignment;
+import com.hcc.entities.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -9,4 +10,25 @@ public class AssignmentController {
 
     //since this handles http requests, should @getmapping only be mentioned
     // here?
+
+
+    @GetMapping("/api/assignments")
+    public static Assignment getAssignmentById(long assignmentId){
+        return null;
+    }
+
+    @GetMapping("/api/assignments/{id}")
+    public static Assignment getAssignmentByUser(User user){
+        return null;
+    }
+    @PutMapping("/api/assignments/{id}")
+    public static boolean putAssignmentsById(long assignmentId){
+        return false;
+    }
+
+    @PostMapping("/api/assignments")
+    public static boolean postAssignmentsById(long assignmentId){
+        return false;
+    }
+
 }
